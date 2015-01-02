3290db31f5edcfe4f011d9a7cabf5c12cc8fefae
JakobStruye <jakob.struye@hotmail.com>
2015-01-02 17:31:23 +0100

    Some more minor additions, project is finished I think

--- elements/local/visitorlist.cc
+++ elements/local/visitorlist.cc
@@ -189,8 +189,9 @@ void VisitorList::push(int input, Packet *p){
         ip_header->ip_dst = entry->ip_src;
         udp_header->uh_sport = htons(434);
         udp_header->uh_dport = htons(entry->port_src);
-        if (reply->code == 1)
+        if (reply->code == 1) {
             click_chatter("Foreign Agent: Visitor List updated after receiving Registration Reply (accepted)");
+        }
         else {
             click_chatter("Foreign Agent: Visitor List updated after receiving Registration Reply (denied)");
             deleteEntry(entry);
